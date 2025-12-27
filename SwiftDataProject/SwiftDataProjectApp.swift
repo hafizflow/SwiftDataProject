e2f94fadfbae30d63aa0f66bee8acc3682fb984c
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-12-28 01:09:04 +0600

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Hafizur Rahman on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
